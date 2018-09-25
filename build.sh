eff503198b92de9604e2e1808f6fc5d514024ca5
XLogosX <none@world.com>
2018-09-25 14:51:47 +0200

    Structure adjusted according to examples + Req1-4 adjusted

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#pdflatex srs.tex
+##makeindex srs.tex
+#makeglossaries srs.tex
+#pdflatex srs.tex
+
 pdflatex srs.tex
+makeindex srs.tex
+#bibtex srs.tex
+biber srs.tex
+makeglossaries srs
+#biber srs.tex
 pdflatex srs.tex
-rm *.aux *.bcf *.log *.out *.run*
+rm *.a* *.bcf *.log *.out *.run* *.g* *.i* *.toc
